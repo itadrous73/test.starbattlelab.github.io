8c10baebaf6dbe75fec2b1e81146acab00415ef1
Isaiah Tadrous <90001927+itadrous73@users.noreply.github.com>
2025-09-04 13:05:02 -0400

    Update service-worker.js

--- Main/mobile/service-worker.js
+++ Main/mobile/service-worker.js
@@ -15,7 +15,7 @@
 
 // --- SERVICE WORKER CONFIGURATION ---
 
-const CACHE_NAME = 'star-battle-cache-v0.0.0';
+const CACHE_NAME = 'star-battle-cache-v1.0.0';
 const DYNAMIC_CACHE_NAME = 'star-battle-dynamic-v0.0.0';
 
 // --- COMPLETE LIST OF ASSETS TO CACHE FOR OFFLINE USE ---
