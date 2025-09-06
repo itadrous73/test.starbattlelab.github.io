5ca5f5135e84f9e1152eaa09f76d017cc44d2758
Isaiah Tadrous <90001927+itadrous73@users.noreply.github.com>
2025-09-06 13:29:56 +0000

    Update service-worker.js

--- Main/mobile/service-worker.js
+++ Main/mobile/service-worker.js
@@ -27,7 +27,7 @@
  * - All SnapGrid scripts from '/SnapGridScripts/'
  * - The manifest.json file
  */
-const CACHE_NAME = 'star-battle-cache-v3.2.0';
+const CACHE_NAME = 'star-battle-cache-v3.2.1';
 
 /**
  * The dynamic cache, used for resources fetched during runtime.
@@ -36,7 +36,7 @@ const CACHE_NAME = 'star-battle-cache-v3.2.0';
  * the user gets the latest version if online, while still providing an
  * offline fallback.
  */
-const DYNAMIC_CACHE_NAME = 'star-battle-dynamic-v3.2.0';
+const DYNAMIC_CACHE_NAME = 'star-battle-dynamic-v3.2.1';
 
 
 
